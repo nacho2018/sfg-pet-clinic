46f0d56d87fef2b311854161650b1ad0c0986a87
nacho2018 <joseigvillanueva@hotmail.com>
2019-01-01 20:33:20 +0000

    Solve broken links for: owners, error# 46

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/vets")
+
 @Controller
 public class VetController {
 
@@ -15,7 +15,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/", "/index", "/index.html"})
+    @RequestMapping({"/vets", "/vets/index", "/vets.html"})
     public String listVets(Model model){
 
         System.out.println("Storing vets...");
