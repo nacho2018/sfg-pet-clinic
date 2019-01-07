f26e2e022654ef87e85e2d68f4dd1ba46eda3885
nacho2018 <joseigvillanueva@hotmail.com>
2019-01-07 21:06:47 +0000

    closes # 39

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package guru.springframework.sfgpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -22,6 +24,9 @@ public class Pet extends BaseEntity{
     private LocalDate birthDate;
 
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
+
 
     public String getName() {
         return name;
@@ -29,6 +34,13 @@ public class Pet extends BaseEntity{
 
     public void setName(String name) {
         this.name = name;
+    }
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 
     public PetType getPetType() {
